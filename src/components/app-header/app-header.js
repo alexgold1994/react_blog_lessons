27642026159655ad0b35ab52940f42c4071f64b1
Alex <alexgold1994@gmail.com>
2021-02-19 20:37:51 +0200

    react_start_learn

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AppHeader = () => {
+    return (
+        <div className="app-header d-flex">
+            <h1>Alex Radchenko</h1>
+            <h2>5 записай, из них понравилось 0</h2>
+        </div>
+    )
+}
+
+export default AppHeader;
